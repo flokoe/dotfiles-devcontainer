c07caa0c1de7beed063bb93ec7e0d5e8bc49e4a7
flokoe <koehler.florian@gmx.de>
2023-08-11 16:33:34 +0200

    Add terraform auto switch function via tfswitch

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -12,4 +12,20 @@ if status is-interactive
     dircolors -c | source
 
     starship init fish | source
+
+    function switch_terraform --on-event fish_postexec
+        string match --regex '^cd\s' "$argv" > /dev/null
+        set --local is_command_cd $status
+
+        if test $is_command_cd -eq 0 
+            if count *.tf > /dev/null
+                grep -c "required_version" *.tf > /dev/null
+                set --local tf_contains_version $status
+                
+                if test $tf_contains_version -eq 0      
+                    command tfswitch
+                end
+            end
+        end
+    end
 end
