ca5b2f9e88fbb6c468f30f4a07be5ae7568dada6
flokoe <koehler.florian@gmx.de>
2023-08-11 12:26:05 +0200

    Add install script

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+export DEBIAN_FRONTEND=noninteractive
+
+# Update os
+sudo apt-get update
+sudo apt-get upgrade -y
+
+# Ensure all requirements are installed
+sudo apt-get install -y git curl stow vim fish nvim
+
+# Install starship
+curl -sS https://starship.rs/install.sh | sh
+
+# Enable dotfiles
+stow fish starship
+
+# Install fisher plugin manager
+fish -c "curl -sL https://git.io/fisher | source && fisher update"
+
+# Set fish as default shell
+FISH="$(which fish)"
+sudo chsh --shell "$FISH" "$USER"
