c0e18a0307fa8b074e0f6ca5483bf3d2f48eba34
flokoe <koehler.florian@gmx.de>
2023-08-11 12:25:58 +0200

    Add fish config

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,13 @@
+# Commands to run in interactive sessions can go here
+if status is-interactive
+    # Disable greeting on startup
+    set -U fish_greeting
+
+    # Set important variables
+    set -gx EDITOR nvim
+
+    # Enable colors for ls in kitty
+    dircolors -c | source
+
+    starship init fish | source
+end
