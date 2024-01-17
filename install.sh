9f5edb935086cde38502665e1e6841e6269c4f16
flokoe <koehler.florian@gmx.de>
2024-01-17 22:22:38 +0100

    Add functionality to remove existing default configs

--- install.sh
+++ install.sh
@@ -16,8 +16,36 @@ sudo apt-get install -y git curl stow vim fish neovim kitty
 # Install starship
 curl -sS https://starship.rs/install.sh | sh -s -- --yes
 
-# Enable dotfiles
-stow fish starship
+# Enable dotfiles for the following packages
+PACKAGES=(fish starship)
+
+# Remove existing default configs so stow does not fail
+for p in "${PACKAGES[@]}"; do
+    # Get list of all files within packages
+    files="$(find "$p" -mindepth 1)"
+
+    # Remove leading package name from all files
+    files="${files//${p}\//}"
+
+    # Replace dot- syntax with actual dot in file path
+    files="${files//dot-/.}"
+
+    for f in $files; do
+        # Construct full file path
+        f="${HOME}/${f}"
+
+        if [[ $f == *"$p"* ]]; then
+            # Remove existing config
+            echo "Remove the following path: ${f}"
+            rm -rf "$f"
+
+            break
+        fi
+    done
+done
+
+# Link dotfiles aka install packages via stow
+stow "${PACKAGES[@]}"
 
 # Install fisher plugin manager
 fish -c "curl -sL https://git.io/fisher | source && fisher update"
